8f26180fd6ed3ac4522f64a4a60745616e7fa6fe
Panchorn <nonpcn@gmail.com>
2020-06-12 02:29:31 +0700

    Set profile to develop only

--- src/main/kotlin/com/nonpcn/asciidocumentation/landmarkservice/document/StaticResourceConfiguration.kt
+++ src/main/kotlin/com/nonpcn/asciidocumentation/landmarkservice/document/StaticResourceConfiguration.kt
@@ -1,11 +1,13 @@
 package com.nonpcn.asciidocumentation.landmarkservice.document
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.resource.PathResourceResolver
 
 @Configuration
+@Profile("develop")
 internal class StaticResourceConfiguration : WebMvcConfigurer {
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
